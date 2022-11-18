cd44c4c0ccfbf409d3b1c73875e3e5a60d74b8c1
lauralves <105663730+lauralves@users.noreply.github.com>
2022-11-18 18:34:40 -0300

    Inline lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,9 +19,9 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		double min = 100.0; //possível o próprio usuário inserir
-		Predicate<Product> pred = p -> p.getPrice()  >= min; 
+ 
 
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice()  >= min);
 		
 		for (Product p: list) {
 			System.out.println(p);
