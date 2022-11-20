9a548e504c703df0a60b886c7115f59f77246101
lauralves <105663730+lauralves@users.noreply.github.com>
2022-11-20 16:23:12 -0300

    Inline lambda expression

--- src/application/ProgramFunction.java
+++ src/application/ProgramFunction.java
@@ -21,9 +21,9 @@ public class ProgramFunction {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Function <Product, String> func = p -> p.getName().toUpperCase();
+		//Function <Product, String> func = p -> p.getName().toUpperCase();
 		//criando uma nova list para pegar todos os nomes de Product com uppercase
-		List<String> names = list.stream().map(func ).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
